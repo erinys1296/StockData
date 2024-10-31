cf2cbd52faf898acd42283d00cf4683e9c4e5561
erinys1296 <erinys1296@gmail.com>
2024-10-31 11:44:13 +0800

    Update retirement.py

--- retirement.py
+++ retirement.py
@@ -6,7 +6,8 @@ import numpy as np
 st.title('退休金計算器')
 
 # 輸入退休年齡及餘命年限
-retirement_age = st.number_input('請輸入退休年齡', min_value=60, max_value=65, value=61)
+retirement_year = st.number_input('請輸入退休年齡(年)', min_value=60, max_value=65, value=60)
+retirement_month = st.number_input('請輸入退休年齡（月）', min_value=0, max_value=12, value=11)
 life_expectancy = st.number_input('請輸入餘命年限', min_value=65, max_value=100, value=78)
 
 
@@ -17,7 +18,7 @@ sy = (35 *12 + 2)/12
 
 Q2 = (( 45800 * (sy + (y-60)) * 0.0155 ) * (1-(5-(y-60))*(4/100)))
 
-
+retirement_age = retirement_year + retirement_month/12
 
 # 計算每月可領的退休金
 
@@ -25,8 +26,8 @@ monthly_pension = (( 45800 * (sy + (retirement_age-60)) * 0.0155 ) * (1-(5-(reti
 years_pension = monthly_pension * (life_expectancy - retirement_age)*12
 
 # 顯示每月可領的退休金
-st.write(f'每月可領的退休金: {monthly_pension:.2f} 元')
-st.write(f'總共可領的退休金: {years_pension:.2f} 元')
+st.write(f'每月可領的退休金: {monthly_pension:.0f} 元')
+st.write(f'總共可領的退休金: {years_pension:.0f} 元')
 
 
 
